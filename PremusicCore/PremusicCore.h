87b6d781d044037adb4cb39f69aed7d0d6e599ae
sora0077 <t.hayashi0077+github@gmail.com>
2017-07-27 01:57:01 +0900

    Add xcodeproj

---
+++ PremusicCore/PremusicCore.h
@@ -0,0 +1,19 @@
+//
+//  PremusicCore.h
+//  PremusicCore
+//
+//  Created by 林達也 on 2017/07/27.
+//  Copyright © 2017年 jp.sora0077. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PremusicCore.
+FOUNDATION_EXPORT double PremusicCoreVersionNumber;
+
+//! Project version string for PremusicCore.
+FOUNDATION_EXPORT const unsigned char PremusicCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PremusicCore/PublicHeader.h>
+
+
