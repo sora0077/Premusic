4b6ab8a6fa438e97c52840b8da81eaf0d324cee7
sora0077 <t.hayashi0077+github@gmail.com>
2017-09-16 14:28:09 +0900

    Refactor directory

---
+++ PremusicCore/Sources/Module/SelectStorefront/SelectStorefront.swift
@@ -0,0 +1,11 @@
+//
+//  SelectStorefront.swift
+//  PremusicCore
+//
+//  Created by 林達也 on 2017/09/13.
+//  Copyright © 2017年 jp.sora0077. All rights reserved.
+//
+
+import Foundation
+
+public struct SelectStorefront {}
